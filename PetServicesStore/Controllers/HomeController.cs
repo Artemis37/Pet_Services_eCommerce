e405c1dfdf9d6d77c049ec1ff76be2180809faa2
Artemis <xuyenchixanh1976@gmail.com>
2021-07-15 17:15:15 +0700

    Web Layout

---
+++ PetServicesStore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using PetServicesStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetServicesStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var temp = new Test() { id=1, name="Iron Man" };
+            return View(temp);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PetServicesStore/Models/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetServicesStore.Models
+{
+    public class Test
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public Test()
+        {
+        }
+        public Test(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
