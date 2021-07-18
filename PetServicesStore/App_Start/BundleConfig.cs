54cd5bff546e023f5a68f068bcbc4a33123604d1
Artemis <xuyenchixanh1976@gmail.com>
2021-07-18 11:19:27 +0700

    Finish Front End

--- PetServicesStore/App_Start/BundleConfig.cs
+++ PetServicesStore/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace PetServicesStore
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+                        "~/Scripts/popper.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -25,6 +28,11 @@ namespace PetServicesStore
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/css/style-starter.css",
+                      "~/Content/css/login.css"));
         }
     }
 }
